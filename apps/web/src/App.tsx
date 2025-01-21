5474f9bba29f2de6294a3c9c3c9929ede4d28ed3
Joan Vilarrasa <joan.vilarrasa96@gmail.com>
2025-01-21 18:41:30 +0100

    added shadcn and tailwind and got the first render for the web

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,23 +1,61 @@
-import { useCallback, useEffect } from 'react';
-import { ReactFlow, useNodesState, useEdgesState, addEdge, type Connection,} from '@xyflow/react';
+import { useCallback, useEffect, useMemo } from 'react';
+import { type NodeProps as RF_NodeProps, type Node as RF_Node, ReactFlow, useNodesState, useEdgesState, addEdge, type Connection,} from '@xyflow/react';
 import demoGraph from './assets/demo-graph.json';
 import '@xyflow/react/dist/style.css';
+import { InterfaceNodeComponent } from './components/InterfaceNode';
+import type { NodeType, Node, Graph } from '@ts2uml/models';
 
 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
+// const initialNodes = [
+//   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+//   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+// ];
+
+const initialGraph = demoGraph as Graph;
+console.log(initialGraph);
+const initialNodes: RF_Node<{data: Node}>[] = [];
+
+let initialX = 0;
+let initialY = 0; 
+for (const node of initialGraph.nodes) {
+  initialNodes.push({ id: node.id, type: "interface", position: { x: initialX, y: initialY }, data: { data: node } });
+  initialX += 100;
+  initialY += 100;
+}
+
+
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
+// Export a component for each node type
+type CustomNodeComponents = {[K in NodeType]: (props: RF_NodeProps<RF_Node<{data: Node}>>) => JSX.Element }
+
 export default function App() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const nodeTypes = useMemo<CustomNodeComponents>(() => ({
+    interface: InterfaceNodeComponent,
+    class: InterfaceNodeComponent,
+    enum: InterfaceNodeComponent, 
+    function: InterfaceNodeComponent,
+    type: InterfaceNodeComponent,
+    variable: InterfaceNodeComponent
+  }), []);
+  
+  const [nodes, setNodes, onNodesChange] = useNodesState<RF_Node>(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-
   async function createDefaultGraph() {
-    const initialGraph = demoGraph;
-    console.log(initialGraph);
+    // const initialGraph = demoGraph as Graph;
+    // console.log(initialGraph);
+    // const newRFNodes: RF_Node<{data: Node}>[] = [];
+
+    // let initialX = 0;
+    // let initialY = 0; 
+    // for (const node of initialGraph.nodes) {
+    //   newRFNodes.push({ id: node.id, position: { x: initialX, y: initialY }, data: { data: node } });
+    //   initialX += 100;
+    //   initialY += 100;
+    // }
+
+    // setNodes(newRFNodes);
   }
 
   useEffect(() => {
@@ -37,6 +75,7 @@ export default function App() {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        nodeTypes={nodeTypes}
       />
     </div>
   );
---
+++ apps/web/src/components/InterfaceNode.tsx
@@ -0,0 +1,30 @@
+import { type NodeProps as RF_NodeProps, type Node as RF_Node, Handle as RF_Handle, Position as RF_Position } from '@xyflow/react';
+import type { Node } from '@ts2uml/models';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from './ui/card';
+
+export function InterfaceNodeComponent(props: RF_NodeProps<RF_Node<{data: Node;}>>) {
+    const node = props.data.data;
+    const title = props.data.data.title;
+    const attributes = props.data.data.attributes;
+
+    return (
+
+        <Card>
+            <CardHeader>
+                <CardDescription>{`<<${title.nodeType}>>`}</CardDescription>
+                <CardTitle>{title.text}</CardTitle>
+            </CardHeader>
+            <CardContent>
+                {attributes.map((attribute, index) => (
+                    <div key={`${node.id}-${index}`}>
+                        <p>{attribute.text}</p>
+                        <RF_Handle type="source" position={RF_Position.Right} id={`${node.id}-${index}-${RF_Position.Right}`} />
+                        <RF_Handle type="source" position={RF_Position.Bottom} id={`${node.id}-${index}-${RF_Position.Left}`} />
+                    </div>
+                ))}
+            </CardContent>
+        </Card>
+    );
+}
+
+
